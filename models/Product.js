23789106c778f6e7bbae8d2b498c5032f715d0dc
pashag123 <pashag123@gmail.com>
2023-11-14 13:28:00 -0800

    created files and begun

---
+++ models/Product.js
@@ -0,0 +1,40 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
+
+// set up fields and rules for Product model
+Product.init(
+  {
+    product_name: {
+type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
+  },
+  {
+    // define columns
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+
+module.exports = Product;
